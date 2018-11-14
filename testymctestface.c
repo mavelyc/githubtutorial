fcf2736b03d99aead739243a3bc2d71c6551fe50
danielwise14 <danielwise14@gmail.com>
2018-11-14 18:03:17 -0500

    made new file

---
+++ testymctestface.c
@@ -0,0 +1,8 @@
+//
+//  testymctestface.c
+//  
+//
+//  Created by Daniel Wise on 2018-11-14.
+//
+
+#include "testymctestface.h"
---
+++ testymctestface.h
@@ -0,0 +1,13 @@
+//
+//  testymctestface.h
+//  
+//
+//  Created by Daniel Wise on 2018-11-14.
+//
+
+#ifndef testymctestface_h
+#define testymctestface_h
+
+#include <stdio.h>
+
+#endif /* testymctestface_h */
